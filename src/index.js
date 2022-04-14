39aac924409653e6215ae33720bbd2fdad817ec6
praveen-1995 <praveenkumarthipparam@gmail.com>
2022-04-13 21:30:07 -0500

    JSX CSS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css"
 
-function Greeting(){
-  return <h1>This is the greeting component</h1>;
+//Explicit return
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book></Book>
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting></Greeting>, document.getElementById("root"));
+const author = "Amelia Hepworth";
+
+//Implicit return
+const Book = () => {
+  const title = "I Love You to the Moon and Back";
+  return (
+    <article className="book">
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg"
+        alt="book-img"
+      ></img>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+}
+
+ReactDOM.render(<BookList></BookList>, document.getElementById("root"));
