7e0ccf916f68f19be5f34514e82b9537b3a1ccaa
praveen-1995 <praveenkumarthipparam@gmail.com>
2022-04-14 23:05:20 -0500

    Props Children

--- src/index.js
+++ src/index.js
@@ -1,46 +1,56 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"
+import "./index.css";
 
 //Creating the variables
 const firstBook = {
   img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg",
   title: "I Love You to the Moon and Back",
-  author: "Amelia Hepworth"
-}
+  author: "Amelia Hepworth",
+};
 
 const secondBook = {
   img: "https://images-na.ssl-images-amazon.com/images/I/81WZ6QvGZ2L._AC_UL200_SR200,200_.jpg",
   title: "It's Not Easy Being a Bunny",
-  author: "Marilyn Sadler"
-}
+  author: "Marilyn Sadler",
+};
 
 //Explicit return
 function BookList() {
   return (
     <section className="booklist">
       <Book
-        img={firstBook.img} title={firstBook.title} author={firstBook.author}
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+          blanditiis cupiditate ratione id deserunt et nisi autem, quaerat dolor
+          ipsum.
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
       ></Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.author}></Book>
-    </section >
+    </section>
   );
 }
 
 //Implicit return
 const Book = (props) => {
-  const { img, title, author } = props;
+  console.log(props);
+  const { img, title, author, children } = props;
   return (
     <article className="book">
-      <img
-        src={img}
-        alt="book-img"
-      ></img>
+      <img src={img} alt="book-img"></img>
       <h1>{title}</h1>
       <h4>{author}</h4>
-
+      {children}
     </article>
   );
-}
+};
 
 ReactDOM.render(<BookList></BookList>, document.getElementById("root"));
